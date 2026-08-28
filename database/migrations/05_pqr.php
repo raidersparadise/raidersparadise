d968175a6f4d561e920d894662dbde4e84ba4edd
SENA <dylanleonmoreno3@gmail.com>
2026-08-27 19:18:54 -0500

    completadas_migraciones

--- database/migrations/05_pqr.php
+++ database/migrations/05_pqr.php
@@ -6,33 +6,37 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('pqr', function (Blueprint $table) {
             $table->id("id_pqr");
+
             $table->unsignedBigInteger("id_usuario");
             $table->unsignedBigInteger("id_cliente");
+
             $table->string("descripcion_pqr", 255);
-            $table->enum("estado", ["recibida","asignada","en proceso", "requerido","cerrada","rechazada"]);
+
+            $table->enum("estado", [
+                "recibida",
+                "asignada",
+                "en proceso",
+                "requerido",
+                "cerrada",
+                "rechazada"
+            ]);
+
             $table->date("fecha");
 
             $table->foreign("id_usuario")
                   ->references("id_usuario")
                   ->on("usuario");
 
-            $table->foreign("id_cliente") 
-                  ->references("id_cliente")    
+            $table->foreign("id_cliente")
+                  ->references("id_cliente")
                   ->on("cliente");
-
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('pqr');
