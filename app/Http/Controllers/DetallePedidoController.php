3ebc005e4a395bc130f3dc641fe8567f722b13b2
JULY RODRIGUEZ <julynata1999@gmail.com>
2026-09-12 10:49:23 -0500

    requestycontrollerjuly

--- app/Http/Controllers/DetallePedidoController.php
+++ app/Http/Controllers/DetallePedidoController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+//comentarios
+
 use App\Services\DetallePedidoService;
 use App\Http\Requests\DetallePedido\StoreDetallePedidoRequest;
 use App\Http\Requests\DetallePedido\UpdateDetallePedidoRequest;
