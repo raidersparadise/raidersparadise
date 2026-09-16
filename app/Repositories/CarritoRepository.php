ac0e6edda8471a410d79a8de1a6185c39308bf4c
MariaJoseRoldanCuellar <roldancuellarmariajose@gmail.com>
2026-09-15 23:17:02 -0500

    feat/repositorioMajo2

--- app/Repositories/CarritoRepository.php
+++ app/Repositories/CarritoRepository.php
@@ -4,47 +4,18 @@ namespace App\Repositories;
 
 use App\Models\Carrito;
 
-class CarritoRepository
+class CarritoRepository extends BaseRepository
 {
-    // Obtener todos los carritos
-    public function getAll()
+    public function __construct(Carrito $model)
     {
-        return Carrito::all();
+        parent::__construct($model);
     }
 
-    // Obtener carrito por ID
-    public function getById(int $id)
-    {
-        return Carrito::findOrFail($id);
-    }
-
-    // Crear carrito
-    public function create(array $datos)
-    {
-        return Carrito::create($datos);
-    }
-
-    // Actualizar carrito
-    public function update(array $datos, int $id)
-    {
-        $carrito = Carrito::findOrFail($id);
-
-        $carrito->update($datos);
-
-        return $carrito;
-    }
-
-    // Eliminar carrito
-    public function delete(int $id)
-    {
-        $carrito = Carrito::findOrFail($id);
-
-        return $carrito->delete();
-    }
-
-    // Buscar carritos por cliente
     public function getByCliente(int $id_cliente)
     {
-        return Carrito::where('id_cliente', $id_cliente)->get();
+        return $this->model->where(
+            'id_cliente',
+            $id_cliente
+        )->get();
     }
 }
